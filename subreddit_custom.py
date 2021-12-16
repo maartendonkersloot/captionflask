17b2f58dccb8c7ff315b3212b400e460d7267796
maartendonkersloot <maartendonkersloot@gmail.com>
2021-12-16 20:57:57 +0100

    Prepared branch

--- subreddit_custom.py
+++
@@ -1,5 +0,0 @@
-class SubredditCustom:
-    def __init__(self, subreddit_list, name):
-        self.subreddit_list = subreddit_list
-        self.identifier = name
-        
--- tests.py
+++
@@ -1,6 +0,0 @@
-import utils
-import unittest
-import subreddit_custom
-
-
-print(can_i_post_to_bodyswap())
