237a89f6d880f7831e46acbbf086dde82f6e4f32
maartendonkersloot <maartendonkersloot@gmail.com>
2021-12-22 15:30:41 +0100

    Added docs to tests

--- api/tests/test_utils.py
+++ api/tests/test_utils.py
@@ -1,61 +1,79 @@
-import os, sys
-from datetime import datetime
-from datetime import timezone
+"""
+Test class for the utils class
+"""
+import pickle
+import os
+import sys
+from datetime import datetime, timezone
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-import utils
-import pickle
+import utils  # pylint: disable=import-error, wrong-import-position
 
 
 def test_cant_post_with_more_than_3_posts_to_bodyswap():
+    """
+    test if it can post to bodyswap if there are already 3 posts in the past 12 hours.
+    should be false
+    """
     with open("api/tests/testable.pickle", "rb") as handle:
-        b = pickle.load(handle)
-    dt = datetime.now(timezone.utc)
-    utc_time = dt.replace(tzinfo=timezone.utc)
+        test_array = pickle.load(handle)
+    datetime_now = datetime.now(timezone.utc)
+    utc_time = datetime_now.replace(tzinfo=timezone.utc)
     utc_timestamp = utc_time.timestamp()
     index_checker = 0
     for index in range(0, 3):
         index_checker += 1
-        b[index].created_utc = utc_timestamp
-        b[index].subreddit = "bodyswap"
+        test_array[index].created_utc = utc_timestamp
+        test_array[index].subreddit = "bodyswap"
     assert index_checker == 3
-    assert utils.can_i_post_to_bodyswap(b) == False
+    assert utils.can_i_post_to_bodyswap(test_array) is False
 
 
 def test_cant_post_with_more_than_4_posts_to_bodyswap():
+    """
+    Tets if it can post to bodyswap if there are already 4 posts in the past 2 hours.
+    Should be false.
+    """
     with open("api/tests/testable.pickle", "rb") as handle:
-        b = pickle.load(handle)
-    dt = datetime.now(timezone.utc)
-    utc_time = dt.replace(tzinfo=timezone.utc)
+        test_array = pickle.load(handle)
+    datetime_now = datetime.now(timezone.utc)
+    utc_time = datetime_now.replace(tzinfo=timezone.utc)
     utc_timestamp = utc_time.timestamp()
     index_checker = 0
     amount = 4
     for index in range(0, amount):
         index_checker += 1
-        b[index].created_utc = utc_timestamp
-        b[index].subreddit = "bodyswap"
-    assert index_checker == amount
-    assert utils.can_i_post_to_bodyswap(b) == False
+        test_array[index].created_utc = utc_timestamp
+        test_array[index].subreddit = "bodyswap"
+    assert index_checker is amount
+    assert utils.can_i_post_to_bodyswap(test_array) is False
 
 
 def test_can_post_with_more_than_4_posts_with_2_bodyswap_to_bodyswap():
+    """
+    Test if it can post with 2 bodyswap posts in the past 12 hours.
+    It should be able to post.
+    """
     with open("api/tests/testable.pickle", "rb") as handle:
-        b = pickle.load(handle)
-    dt = datetime.now(timezone.utc)
-    utc_time = dt.replace(tzinfo=timezone.utc)
+        test_array = pickle.load(handle)
+    datetime_now = datetime.now(timezone.utc)
+    utc_time = datetime_now.replace(tzinfo=timezone.utc)
     utc_timestamp = utc_time.timestamp()
     index_checker = 0
     amount = 2
     for index in range(0, amount):
         index_checker += 1
-        b[index].created_utc = utc_timestamp
-        b[index].subreddit = "bodyswap"
-    assert index_checker == amount
-    assert utils.can_i_post_to_bodyswap(b) == True
+        test_array[index].created_utc = utc_timestamp
+        test_array[index].subreddit = "bodyswap"
+    assert index_checker is amount
+    assert utils.can_i_post_to_bodyswap(test_array) is True
 
 
 def test_test_if_it_gets_posts():
+    """
+    Test if it can post, if it goes through the can_i_post_to_bodyswap function.
+    """
     assert (
-        utils.can_i_post_to_bodyswap() == True
-        or utils.can_i_post_to_bodyswap() == False
+        utils.can_i_post_to_bodyswap() is True
+        or utils.can_i_post_to_bodyswap() is False
     )
