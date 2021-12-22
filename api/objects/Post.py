6a03983127061290ae46b3993fe0706c532b16a5
maartendonkersloot <maartendonkersloot@gmail.com>
2021-12-22 16:12:05 +0100

    Changed folder name

--- api/objects/Post.py
+++ api/objects/Post.py
@@ -1,13 +1,24 @@
-from sqlalchemy import Table, Column, Integer, String, DateTime
-from sqlalchemy.sql import func
+"""
+Post object for the declerative base
+"""
+from sqlalchemy import Column, Integer, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
+
+
 class Post(Base):
+    """
+    Post object for the declerative base
+    Args:
+        Base ([type]): the base object
+    """
+
     __tablename__ = "posts"
-    id = Column('id', Integer, primary_key = True)
-    title =Column('title', String)
-    link = Column('link', String)
-    posted =  Column('posted', Integer)
-    subreddits =  Column('subreddits', String)
-    scheduled =  Column('scheduled', DateTime)
+    id = Column("id", Integer, primary_key=True)
+    title = Column("title", String)
+    link = Column("link", String)
+    posted = Column("posted", Integer)
+    subreddits = Column("subreddits", String)
+    scheduled = Column("scheduled", DateTime)
+
--- api/objects/model_base.py
+++ api/objects/model_base.py
@@ -1,11 +1,25 @@
+"""
+Abstract model base
+"""
 from abc import ABCMeta, abstractmethod
 
+
 class AbstractModel(metaclass=ABCMeta):
+    """
+    Abstract model base
+
+    Args:
+        metaclass ([type], optional): Abstracth meta class. Defaults to ABCMeta.
+    """
+
     @abstractmethod
     def __init__(self):
         pass
-    
+
     @abstractmethod
     def get_table(self, meta):
-        pass
-
+        """
+        Get the table of the model
+        Args:
+            meta ([type]): The meta object
+        """
