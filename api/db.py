2bc719e400820eae617e9893dcbfef084780c376
maartendonkersloot <maartendonkersloot@gmail.com>
2021-12-27 01:44:37 +0100

    alchemy location change

--- api/db.py
+++ api/db.py
@@ -2,11 +2,49 @@
 The database object/handler for the api
 """
 import json
-from alchemyencoder import AlchemyEncoder
 from objects.Post import Post
 from sqlalchemy import create_engine, MetaData, null
 from sqlalchemy.orm import sessionmaker
 from utils import upload_to_imgur, can_i_post_to_bodyswap, post_reddit
+
+import json
+from sqlalchemy.ext.declarative import DeclarativeMeta
+
+
+class AlchemyEncoder(json.JSONEncoder):
+    """
+    Alchemy API Encoder class
+    Args:
+        json ([type]): json encocder
+    """
+
+    def default(self, o):
+        """
+        Default function to encode objects
+        Args:
+            obj ([type]): object to encode
+
+        Returns:
+            [type]: json object
+        """
+        if isinstance(o.__class__, DeclarativeMeta):
+            # an SQLAlchemy class
+            fields = {}
+            for field in [
+                x for x in dir(o) if not x.startswith("_") and x != "metadata"
+            ]:
+                data = o.__getattribute__(field)
+                try:
+                    json.dumps(
+                        data
+                    )  # this will fail on non-encodable values, like other classes
+                    fields[field] = data
+                except TypeError:
+                    fields[field] = None
+            # a json-encodable dict
+            return fields
+
+        return json.JSONEncoder.default(self, o)
 
 
 class Db:
